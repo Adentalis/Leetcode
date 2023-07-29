90eb381e2189ce38cc3d8f7bc1a1f32b42e07e88
Adentalis <daniel.n96@t-online.de>
2023-07-29 13:54:07 +0200

    do 217

---
+++ python/217_contains_duplicate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        num_set = set()
+
+        for num in nums:
+            if num in num_set:
+                return True
+            else:
+                num_set.add(num)
+
+        return False
+
+
+s = Solution()
+print(s.containsDuplicate([1, 2, 3, 1]))
