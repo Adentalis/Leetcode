a53cf056dda4ed10d70a70d80d76f58b3852ee14
Adentalis <daniel.n96@t-online.de>
2023-08-06 20:42:37 +0200

    do 20

--- python/20_move_zeroes.py
+++ python/20_move_zeroes.py
@@ -1,10 +1,20 @@
 class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
-        print('tu')
-        for i, e in enumerate(nums):
-            if nums[i] == 0:
-                print(nums)
+        zero_counter = nums.count(0)
+        if zero_counter != 0:
+            zeroes_found = 0
+            for i in range(len(nums)):
+
+                if nums[i] == 0:
+                    zeroes_found += 1
+
+                else:
+                    if zeroes_found != 0:
+                        nums[i - zeroes_found] = nums[i]
+                        nums[i] = 0
+        print(nums)
 
 
 s = Solution()
-s.moveZeroes([0, 2, 3, 0, 0, 8])
+s.moveZeroes([1, 0, 2, 4, 3, 0, 0, 9, 0, 8])
+s.moveZeroes([1, 0])
