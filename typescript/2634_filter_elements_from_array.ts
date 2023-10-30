049450a585609337912153bf2896092c65ae34ed
Adentalis <daniel.n96@t-online.de>
2023-10-30 14:44:44 +0100

    do 2634

---
+++ typescript/2634_filter_elements_from_array.ts
@@ -0,0 +1,12 @@
+type Fn = (n: number, i: number) => any;
+
+function filter(arr: number[], fn: Fn): number[] {
+  let res: number[] = [];
+
+  arr.forEach((e, i) => {
+    if (fn(e, i)) {
+      res.push(e);
+    }
+  });
+  return res;
+}
